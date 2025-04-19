cea769f8048d07a0afd05362faedf459217efa27
Jibril Daulay <141029020+Rilj@users.noreply.github.com>
2025-04-19 16:30:20 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// satu website pop-out animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+
+hiddenElements.forEach((el) => observer.observe(el));
